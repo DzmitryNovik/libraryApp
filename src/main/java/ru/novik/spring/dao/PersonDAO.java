53d2235e002ca976e4a06e581e19c2c54d9e4809
DzmitryN <nowikmitya@yandex.ru>
2021-11-28 11:52:10 +0300

    first comment

---
+++ src/main/java/ru/novik/spring/dao/PersonDAO.java
@@ -0,0 +1,57 @@
+package ru.novik.spring.dao;
+
+import org.springframework.stereotype.Component;
+import ru.novik.spring.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(PEOPLE_COUNT++, "Tom"));
+        people.add(new Person(PEOPLE_COUNT++, "Bob"));
+        people.add(new Person(PEOPLE_COUNT++, "Carl"));
+        people.add(new Person(PEOPLE_COUNT++, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        /*Person tempPerson = null;
+        for (Person person : people) {
+            if (id == person.getId()) {
+                tempPerson = people.get(id);
+            }
+        }
+        return tempPerson;*/
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(PEOPLE_COUNT++);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        /*for (int i = 0; i < people.size(); i++) {
+            if (id == people.indexOf(people.get(i))) {
+                people.remove(i);
+            }
+        }*/
+        people.removeIf(p -> p.getId() == id);
+    }
+}
