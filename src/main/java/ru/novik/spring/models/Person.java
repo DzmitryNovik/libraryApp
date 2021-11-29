81e9227b785a6003123b820372b48b4f05409c8b
DzmitryN <nowikmitya@yandex.ru>
2021-11-29 16:40:37 +0300

    Changes

--- src/main/java/ru/novik/spring/models/Person.java
+++ src/main/java/ru/novik/spring/models/Person.java
@@ -1,18 +1,33 @@
 package ru.novik.spring.models;
 
-import java.util.Objects;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 public class Person {
-
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
     public Person() {
+
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -31,17 +46,19 @@ public class Person {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return id == person.id &&
-                Objects.equals(name, person.name);
+    public int getAge() {
+        return age;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
